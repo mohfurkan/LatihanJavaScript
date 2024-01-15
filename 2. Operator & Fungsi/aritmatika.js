f0546fbb5f50522fd12a547d6d9fffd49f606d71
unknown <furkantahede@gmail.com>
2024-01-15 14:33:55 +0800

    Operato & Fungsi

---
+++ 2. Operator & Fungsi/aritmatika.js
@@ -0,0 +1,38 @@
+let bill = 10;
+let bill = 3;
+
+//operator penjumlahan
+let kurang;
+jumlah = bil1 + bil2;
+console.log(jumlah);
+
+//operator pengurangan
+let kurang;
+kurang = bil1 - bil2;
+console.log(kurang)
+
+//operator perkalian
+let kali;
+kali = bil1 + bil2;
+console.log(kali)
+
+//operator pembagian
+let bagi;
+bagi = bil1 / bil2;
+console.log(bagi);
+
+//eksponen (pangkat)
+let pangkat;
+pangkat = bil1 ** bil2;
+console,log(pangkat);
+
+//modulus (sisa hasil bagi)
+let modulus;
+modulus = bil % bil2;
+console.log(modulus);
+
+//increment | decrement
+bil1++;
+console.log(bil1);
+bil2--;
+console.log(bil2);
